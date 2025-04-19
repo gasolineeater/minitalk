1dd9a44b10444af9091d9f15c78a344d2f073856
gasolineeater <ezekaj@student.42heilbronn.de>
2025-04-19 20:07:49 +0200

    Edit: paths for libft.

---
+++ minitalk.h
@@ -0,0 +1,10 @@
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include "Libft/libft.h"
+# include "Libft/printf/ft_printf.h"
+# include "Libft/gnl/get_next_line.h"
+# include <signal.h>
+# include <stdlib.h>
+
+#endif
