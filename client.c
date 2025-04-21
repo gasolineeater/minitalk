2cb9bb7a50ea466ae2fa07b9c92fb4bd669c150b
gasolineeater <ezekaj@student.42heilbronn.de>
2025-04-21 15:10:23 +0200

    Add: send_msg function that sends the string char by char and check for the 1 and 0 using bit mask.

--- client.c
+++ client.c
@@ -6,11 +6,32 @@
 /*   By: ezekaj <ezekaj@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/19 17:37:25 by ezekaj            #+#    #+#             */
-/*   Updated: 2025/04/21 14:35:13 by ezekaj           ###   ########.fr       */
+/*   Updated: 2025/04/21 15:04:18 by ezekaj           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+void	send_msg(pid_t pid, char *msg)
+{
+	int		bit_mask;
+	char	curr_char;
+
+	while (*msg)
+	{
+		curr_char = *msg++;
+		bit_mask = 0b10000000;
+		while (bit_mask)
+		{
+			if (curr_char & bit_mask)
+				kill(pid, SIGUSR1);
+			else
+				kill(pid, SIGUSR2);
+			bit_mask >>= 1;
+			usleep(500);
+		}
+	}
+}
 
 int	main(int ac, char **av)
 {
@@ -29,6 +50,7 @@ int	main(int ac, char **av)
 		return (EXIT_FAILURE);
 	}
 	msg = ft_strdup(av[2]);
+	send_msg(pid, msg);
 	free(msg);
 	return (EXIT_SUCCESS);
 }
