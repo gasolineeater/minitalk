6ba1335c14b26be52a9d15b993b6af328c5fdd3a
gasolineeater <ezekaj@student.42heilbronn.de>
2025-04-21 14:35:04 +0200

    Add: Edge cases for not enough arguments and if pid is valid.

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: ezekaj <ezekaj@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/19 17:37:25 by ezekaj            #+#    #+#             */
-/*   Updated: 2025/04/19 20:04:51 by ezekaj           ###   ########.fr       */
+/*   Updated: 2025/04/21 14:34:15 by ezekaj           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,19 @@
 
 int	main(int ac, char **av)
 {
-    (void) ac;
-    (void) av;
-	ft_printf("Client started\n");
-    
-	return (0);
+	char	*msg;
+	int		pid;
+
+	if (ac != 3)
+	{
+		ft_printf("Usage: %s <pid> <message>\n", av[0]);
+		return (EXIT_FAILURE);
+	}
+	pid = ft_atoi(av[1]);
+	if (pid <= 0)
+	{
+		ft_printf("Error: Invalid pid.\n");
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
 }
